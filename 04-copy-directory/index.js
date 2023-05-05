078d9a04aa01892c509bcae7f575350fa475bf29
Strabuk <119878326+Strabuk@users.noreply.github.com>
2023-05-05 14:32:43 +0300

    fix: delete file to task copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,6 +4,16 @@ const fs = require ('fs');
 fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, error => {
  if (error) throw error;
 });
+
+fs.readdir(path.join(__dirname, 'files-copy'), (error, files) => {
+ if (error) throw error;
+ for ( const file of files) {
+  fs.unlink(path.join(__dirname, 'files-copy', file), error => {
+   if (error) throw error;
+  });
+ };
+});
+
 fs.readdir(path.join(__dirname, 'files'), (error, files) => {
  if (error) throw error;
  for ( const file of files) {
@@ -11,4 +21,4 @@ fs.readdir(path.join(__dirname, 'files'), (error, files) => {
   if (error) throw error;
  });
  };
-})
+});
