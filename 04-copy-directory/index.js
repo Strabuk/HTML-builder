e94c0382d411dbd572680116dd9890f917cbbe88
Strabuk <119878326+Strabuk@users.noreply.github.com>
2023-05-05 14:16:30 +0300

    fix: done task copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,14 @@
+const path = require ('path');
+const fs = require ('fs');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, error => {
+ if (error) throw error;
+});
+fs.readdir(path.join(__dirname, 'files'), (error, files) => {
+ if (error) throw error;
+ for ( const file of files) {
+ fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), error => {
+  if (error) throw error;
+ });
+ };
+})
